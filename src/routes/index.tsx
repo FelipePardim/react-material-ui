421cb589904f0332e5532ad35051cc3e7bee5bba
FelipePardim <felipe.santos@ciss.com.br>
2023-08-04 10:45:41 -0300

    feat: adding mui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Pagina inical</p>}></Route>
+            <Route path="/pagina-inicial" element={<Button>Teste</Button>}></Route>
 
             <Route path="*" element={<Navigate to="/pagina-inicial"/>}></Route>
         </Routes>
