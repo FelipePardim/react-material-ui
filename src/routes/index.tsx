dcf403dc229af256f8317f92b74afc7d19aea2bd
FelipePardim <felipe.santos@ciss.com.br>
2023-08-07 15:54:14 -0300

    feat: base layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([{
@@ -16,7 +16,7 @@ export const AppRoutes = () => {
     
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Toggle Drawer</Button>}></Route>
+      <Route path="/pagina-inicial" element={<Dashboard />}></Route>
 
       <Route path="*" element={<Navigate to="/pagina-inicial"/>}></Route>
     </Routes>
