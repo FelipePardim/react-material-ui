f4432aece518a42b38f61f10fa45381be843e5f4
FelipePardim <felipe.santos@ciss.com.br>
2023-08-14 15:06:20 -0300

    feat: adding pessoas crud service

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -39,15 +39,69 @@ const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Er
   }
 };
 
-const getById = async (): Promise<any> => {};
+const getById = async (id: number): Promise<IDetalhePessoa | Error> => {
+  try {
+    const urlRelativa = `/pessoas/${id}`;
+    const { data } = await Api.get(urlRelativa);
 
-const create = async (): Promise<any> => {};
+    if (data) {
+      return data;
+    }
 
-const updateById = async (): Promise<any> => {};
+    return new Error('Erro ao buscar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as {message: string}).message || 'Erro ao buscar o registro');
+  }
+};
 
-const deleteById = async (): Promise<any> => {};
+const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error> => {
+  try {
+    const urlRelativa = '/pessoas';
+    const { data } = await Api.post<IDetalhePessoa>(urlRelativa, dados);
+    
+    if (data) {
+      return data.id;
+    }
+    
+    return new Error('Erro ao criar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as {message: string}).message || 'Erro ao criar o registro.');
+  }
+};
 
+const updateById = async (dados: IDetalhePessoa, id: number): Promise<number | Error> => {
+  try {
+    const urlRelativa = `/pessoas/${id}`;
+    const { status } = await Api.put<IDetalhePessoa>(urlRelativa, dados);
 
+    if (status === 200) {
+      return status;
+    }
+
+    return new Error('Erro ao atualizar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as {message: string}).message || 'Erro ao atualizar o registro.');
+  }
+};
+
+const deleteById = async (id: number): Promise<number | Error> => {
+  try {
+    const urlRelativa = `/pessoas/${id}`;
+    const { status } = await Api.delete(urlRelativa);
+
+    if (status === 200) {
+      return status;
+    }
+  
+    return new Error('Erro ao apagar o registro.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as {message: string}).message || 'Erro ao apagar registro');
+  }
+};
 
 export const PessoasService = {
   getAll,
