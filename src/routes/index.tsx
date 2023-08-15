021db997dd20dee461b6eead415bce70c22140b4
FelipePardim <felipe.santos@ciss.com.br>
2023-08-15 14:17:35 -0300

    feat: adding cities page and navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,30 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDeCidade } from '../pages';
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
-    setDrawerOptions([{
-      label: 'Página inicial',
-      icon: 'home',
-      path: '/pagina-inicial',
-    }]);
+    setDrawerOptions([
+      {
+        label: 'Página inicial',
+        icon: 'home',
+        path: '/pagina-inicial',
+      },
+      {
+        label: 'Cidades',
+        icon: 'location_city',
+        path: '/cidades',
+      },
+    ]);
   }, []);
     
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />}></Route>
+      <Route path="/cidades" element={<ListagemDeCidade />}></Route>
 
       <Route path="*" element={<Navigate to="/pagina-inicial"/>}></Route>
     </Routes>
